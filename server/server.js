56cd8e623ef721e416edc0622abb414ab60c0ffe
Levi <lyderen@gmail.com>
2018-05-28 00:12:28 +0300

    mahse

--- server/server.js
+++ server/server.js
@@ -13,18 +13,33 @@ app.use(bodyParser.json());
 
 //getingsunshinetimeaccordinglocation/date=123444,lat=2452323,lan=2323523
 
-app.get('/getingsunshinetimeaccordinglocation/:date/:lat/:lon', (req,res) => {
+app.get('/', (req,res) => {
+  res.send('<h1> woelcom to sunshine <h1> <p> u must send time langtidiu & longtediuo and u get the sunshuine time <p>');
+});
 
-  const time = parseInt(req.params.date);
-  const lat = parseInt(req.params.lat);
+app.post('/getingsunshinetimeaccordinglocation', (req,res) => {
   
-  const lon = parseInt(req.params.lon);
+    const clientTime = parseFloat(req.body.time);
+    const time = new Date(clientTime);
+     const lat = req.body.lat;
+     const lan = req.body.lan;
+    
+     var a = new Date();
 
-  const  times = SunCalc.getTimes(new Date(time),lat,lon);
+  console.log(a.getUTCHours);
+  // const time = parseInt(req.params.date);
+  // const lat = parseInt(req.params.lat);
+  //a.getFullYear(),a.getMonth(),a.getDate() + 1
+  // const lon = parseInt(req.params.lon);
+  console.log(a.getFullYear());
+  console.log(a.getMonth() + 1);
+  console.log(a.getDate());
+  console.log(new Date(a.getFullYear(),a.getMonth() + 1,a.getDate()));
+   const  times = SunCalc.getTimes(new Date(2018,4,28),32.0880577,34.8672868);
   const sunriseStr = times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
   const sunsetstr = times.sunset.getHours() + ':' + times.sunset.getMinutes();
-  console.log(sunriseStr)
-   res.send({sunriseStr,sunsetstr})
+  // console.log(sunriseStr)
+   res.send({times,sunriseStr,sunsetstr})
 
 
 });
