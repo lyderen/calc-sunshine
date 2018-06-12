438fb52c93efedb78e71bc1e8928c5f1da019bba
Levi <lyderen@gmail.com>
2018-06-13 00:38:21 +0300

    adding ACAO1

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.get('/', (req,res) => {
