504b22dcba790a727ac736783194140a6ef9418d
Levi <lyderen@gmail.com>
2018-06-12 21:56:26 +0300

    finnal url

--- server/server.js
+++ server/server.js
@@ -7,23 +7,20 @@ const app = express();
 app.use(bodyParser.json());
 
 
-//1527202356867
-//32.0880577
-//34.8672868
 
-//getingsunshinetimeaccordinglocation/date=123444,lat=2452323,lan=2323523
 
 app.get('/', (req,res) => {
   res.send('<h1> woelcom to sunshine <h1> <p> u must send time langtidiu & longtediuo and u get the sunshuine time <p>');
 });
 
-app.post('/getingsunshinetimeaccordinglocation', (req,res) => {
-    
-    const clientTime = req.body.time;
-    const time = new Date(clientTime);
-     const lat = parseFloat (req.body.lat);
-     const lan = parseFloat(req.body.lan);
+app.get('/getingsunshinetimeaccordinglocation/:time/:lat/:lan', (req,res) => {
+    // req.params.time
+   const clientTime = parseInt(req.params.time);
+    const time = parseInt(req.params.time);
+     const lat = parseFloat(req.params.lat);
+     const lan = parseFloat(req.params.lan);
 
+console.log(time,lat,lan);
    const  times = SunCalc.getTimes(new Date(clientTime),lat,lan);
   const sunriseStr = times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
   const sunsetstr = times.sunset.getHours() + ':' + times.sunset.getMinutes();
@@ -35,4 +32,4 @@ app.post('/getingsunshinetimeaccordinglocation', (req,res) => {
 
 app.listen(port, () =>{
     console.log(`App strating on port ${port}`);
-  });
+  })
